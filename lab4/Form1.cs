b54a829493e5d2a0bb2263c5287ae7380a695937
xcamacho555836-create <x.camacho.555836@umindanao.edu.ph>
2026-05-23 19:05:47 +0800

    done lab 4

--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -1,10 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static lab3.Form1;
+
 namespace lab4
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    [TestClass]
+    public class LibraryTests
+    {
+        [TestMethod]
+        public void TestBookCreation()
+        {
+            Book book = new Book
+            {
+                Title = "C# Programming",
+                Author = "John Smith"
+            };
+            Assert.AreEqual("C# Programming", book.Title);
+            Assert.AreEqual("John Smith", book.Author);
+        }
+
+        [TestMethod]
+        public void TestMagazineCreation()
+        {
+            Magazine magazine = new Magazine
+            {
+                Title = "Tech Weekly",
+                Author = "Jane Doe",
+                IssueNumber = 12
+            };
+            Assert.AreEqual("Tech Weekly", magazine.Title);
+            Assert.AreEqual(12, magazine.IssueNumber);
+        }
+
+        [TestMethod]
+        public void TestEBook_GetInfo()
+        {
+            var ebook = new EBook
+            {
+                Title = "Solo Leveling",
+                Author = "Chu-Gong",
+                Published = "D&C Media"
+            };
+            string expected = "Solo Leveling, Written by Chu-Gong published by D&C Media";
+            Assert.AreEqual(expected, ebook.GetInfo());
+        }
+    }
 }
