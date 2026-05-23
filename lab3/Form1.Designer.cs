04815f2f8de37f34cc6b72e16e70d8f5865b85b0
xcamacho555836-create <x.camacho.555836@umindanao.edu.ph>
2026-05-23 17:41:18 +0800

    Done Week 3

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,52 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnShowInfo = new Button();
+            lbArrayBooks = new ListBox();
+            SuspendLayout();
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.FlatStyle = FlatStyle.Flat;
+            btnShowInfo.Location = new Point(24, 49);
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.Size = new Size(94, 29);
+            btnShowInfo.TabIndex = 0;
+            btnShowInfo.Text = "Show Info";
+            btnShowInfo.UseVisualStyleBackColor = true;
+            btnShowInfo.Click += btnShowInfo_Click;
+            // 
+            // lbArrayBooks
+            // 
+            lbArrayBooks.FormattingEnabled = true;
+            lbArrayBooks.Location = new Point(191, 49);
+            lbArrayBooks.Name = "lbArrayBooks";
+            lbArrayBooks.Size = new Size(582, 264);
+            lbArrayBooks.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 358);
+            Controls.Add(lbArrayBooks);
+            Controls.Add(btnShowInfo);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnShowInfo;
+        private ListBox lbArrayBooks;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,104 @@
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Book
+        {
+            public string? Title { get; set; }
+            public string? Author { get; set; }
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} - Issue {IssueNumber}";
+            }
+        }
+
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            Book myBook = new Magazine
+            {
+                Title = "Tech Weekly",
+                Author = "John Doe",
+                IssueNumber = 45
+            };
+            MessageBox.Show(myBook.GetInfo());
+            Book[] arrayBook =
+               {new Book
+                {
+                 Title = "Calculus",
+                 Author = "Aristotle"
+                },
+                new Magazine
+                {
+                    Title = "New York",
+                    Author = "DeadPool",
+                    IssueNumber = 16
+                },
+                new EBook
+                {
+                    Title = "Lord of The Mysteries",
+                    Author = "CatleFish",
+                    Published = "Yeng Press"
+                },
+                new TextBook 
+                {
+                    Title = "Math for Elementary",
+                    Author = "Prof. Paran",
+                    Subject = "Mathematics"
+                },
+                new AudioBook
+                {
+                    Title = "The Man and the Spider",
+                    Author = "James Bond",
+                    Duration = "1:43 m",
+                    Narrator = "Vinz Clark"
+                }
+            };
+            createArray(arrayBook);
+        }
+        public class EBook : Book
+        {
+            public string? Published { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title}, Written by {Author} published by {Published}";
+            }
+        }
+        private void createArray(Book[] arr)
+        {
+           lbArrayBooks.Items.Clear();
+            foreach (Book b in arr)
+            {
+                lbArrayBooks.Items.Add(b.GetInfo());
+            }
+        }
+        public class TextBook : Book
+        {
+            public string? Subject { get; set; }
+            public override string GetInfo()
+            {
+                return $"Subject {Subject}, Title {Title} Written by {Author}";
+            }
+        }
+        public class AudioBook : Book
+        {
+            public string? Duration { get; set; }
+            public string? Narrator { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title}, Written by {Author} Duration {Duration} Narrator {Narrator}";
+            }
+        }
+    }
 }
